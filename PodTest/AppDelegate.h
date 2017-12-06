00430cd745a02e4569726aa187ebb0f3a53cb28b
Qzhm <qzhm@macdeimac.local>
2017-12-06 22:27:30 +0800

    pod test

---
+++ PodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodTest
+//
+//  Created by Qzhm on 17/2/7.
+//  Copyright © 2017年 Qzhm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PodTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PodTest
+//
+//  Created by Qzhm on 17/2/7.
+//  Copyright © 2017年 Qzhm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
